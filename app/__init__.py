7a4a95a677a0d7b69e9406d839aa9fab4f30f2b0
DarthYogurt <raytochina@gmail.com>
2019-02-26 11:30:59 -0800

    category

--- app/__init__.py
+++ app/__init__.py
@@ -20,12 +20,19 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.mod_auth.controllers import mod_auth as auth_module
+# from app.mod_auth.controllers import mod_auth as auth_module
+from app.mod_tech.controllers import mod_tech as tech_module
+from app.mod_category.controllers import mod_category as category_module
+
+
 
 # Register blueprint(s)
-app.register_blueprint(auth_module)
+# app.register_blueprint(auth_module)
 # app.register_blueprint(xyz_module)
-# ..
+app.register_blueprint(tech_module)
+app.register_blueprint(category_module)
+
+
 
 # Build the database:
 # This will create the database file using SQLAlchemy
